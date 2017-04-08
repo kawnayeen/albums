a65c63accfe54b421dead97105b5d12b6f32de2f
kawnayeen <kawnayeen.08@gmail.com>
2017-04-09 00:39:21 +0600

    Album Summary with Cover Image completed

--- src/components/album.details.js
+++ src/components/album.details.js
@@ -2,12 +2,50 @@
  * Created by kawnayeen on 4/8/17.
  */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './card';
+import CardSection from './card.section';
 
-const AlbumDetails = (props) => (
-    <View>
-        <Text>{props.album.title}</Text>
-    </View>
+const AlbumDetails = ({ album }) => (
+    <Card>
+        <CardSection>
+            <View style={styles.thumbnailContainer}>
+                <Image style={styles.thumbnailStyle} source={{ uri: album.thumbnail_image }} />
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.titleTextStyle}>{album.title}</Text>
+                <Text>{album.artist}</Text>
+            </View>
+        </CardSection>
+        <CardSection>
+            <Image style={styles.coverImageStyle} source={{ uri: album.image }} />
+        </CardSection>
+    </Card>
 );
+
+const styles = {
+    textContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    titleTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        width: 50,
+        height: 50
+    },
+    thumbnailContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    coverImageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+};
 
 export default AlbumDetails;
--- src/components/card.js
+++ src/components/card.js
@@ -4,11 +4,27 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = () => (
-    <View />
+const Card = (props) => (
+    <View style={styles.containerStyle}>
+        {props.children}
+    </View>
 );
 
-// const styles = {
-//   container
-// };
+const styles = {
+  containerStyle: {
+      borderWidth: 1,
+      borderRadius: 2,
+      borderColor: '#ddd',
+      borderBottomWidth: 0,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowRadius: 2,
+      shadowOpacity: 0.1,
+      elevation: 1,
+      marginLeft: 5,
+      marginRight: 5,
+      marginTop: 10
+  }
+};
+
 export default Card;
